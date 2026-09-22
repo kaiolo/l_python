8bf7d54c4f21beca364802371b5078cdda06f0b0
kaiolo <3425503053@qq.com>
2026-09-22 18:23:14 +0800

    数据清洗

---
+++ crawl/data_ preprocessing.py
@@ -0,0 +1,37 @@
+import pandas as pd 
+import re
+movie_data  = pd.read_csv("crawl/movie_original.csv" , usecols=["movie_name" , "movie_data" , "movie_scores" , "movie_lang" ,"movie_len"] , nrows= None)
+
+
+"""
+评分检查
+"""
+assert(  (( 0 <movie_data['movie_scores'])&(movie_data['movie_scores']< 100)).all()  ) , "评分异常"
+
+"""
+日期检查
+"""
+for data in movie_data['movie_data'].values :
+    
+    assert( re.match('^\d{4}-\d{2}-\d{2}$' , data) ) , '日期异常'
+
+"""
+语言检查
+"""
+mask = (movie_data['movie_lang'].isnull())|( movie_data["movie_lang"].str.match(r'\$\d+')) |(movie_data['movie_lang']=='-')
+movie_data.loc[mask , "movie_lang"]="unkonwn"
+
+
+
+"""
+时长检查
+"""
+mask = movie_data["movie_len"].apply(lambda x : isinstance (x , int))
+assert mask.all()
+
+
+movie_data.to_csv("crawl/movie_processed.csv")
+
+# print(movie_data)
+# print(movie_data[movie_data.isnull ()])
+
--- crawl/tesst.py
+++ crawl/tesst.py
@@ -1,34 +1,6 @@
+import pandas as pd 
 import re
 
-def parse_hm(text: str):
-    """
-    解析字符串中的小时(h)和分钟(m)数字
-    支持格式："1h 31m", "*5h*", "*8m*", "2h", "30m"
-    :param text: 待解析字符串
-    :return: (hour, minute)，无对应数字返回 None
-    """
-    # 查找h前面的数字
-    h_match = re.search(r'(\d+)h', text)
-    # 查找m前面的数字
-    m_match = re.search(r'(\d+)m', text)
-
-    hour = int(h_match.group(1)) if h_match else 0
-    minute = int(m_match.group(1)) if m_match else 0
-    len    =  60*hour + minute
-    return len
-
-
-# 测试用例
-if __name__ == "__main__":
-    cases = [
-        "1h 31m",
-        "*5h*",
-        "*8m*",
-        "2h",
-        "30m",
-        "*10h*45m",
-        "no hm here"
-    ]
-    for case in cases:
-        t = parse_hm(case)
-        print(f"{t}m")
+a=pd.Series([1,3,4,5,"print"])
+mask = a.apply(lambda x : isinstance (x , int) )
+assert mask.all()
